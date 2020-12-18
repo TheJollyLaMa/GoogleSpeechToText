f411b1c10fca3e45fe9ec2edc415230e2eefe6bf
The Jolly LaMa <J@caffeinelamanna.com>
2020-12-18 06:52:06 -0500

    Update Speech_to_Text.py

--- Speech_to_Text.py
+++ Speech_to_Text.py
@@ -11,7 +11,7 @@ import transcribe_functions as file_handler
 
 file_to_convert = file_handler.get_from_YouTube()
 wav_file, mono_file, txt_file = file_handler.convert_to_mono_wav(file_to_convert)
-bucket_name = "wisdom_bucket"
+bucket_name = "box_of_rain"
 file_to_transcribe = 'gs://' + bucket_name + '/' + mono_file
 file_handler.upload_to_bucket(mono_file,file_to_transcribe)
 transcript = file_handler.SayWhat(file_to_transcribe)
